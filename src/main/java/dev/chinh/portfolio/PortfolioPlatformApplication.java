09ef5472780d432817b0b883c308088f0b2a150e
chinhnt2 <chinhnt2@sapo.vn>
2026-03-13 21:20:57 +0700

    fix: exclude OAuth2 auto-config to resolve 403

--- src/main/java/dev/chinh/portfolio/PortfolioPlatformApplication.java
+++ src/main/java/dev/chinh/portfolio/PortfolioPlatformApplication.java
@@ -2,8 +2,13 @@ package dev.chinh.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    OAuth2ClientAutoConfiguration.class,
+    OAuth2ResourceServerAutoConfiguration.class
+})
 public class PortfolioPlatformApplication {
 
 	public static void main(String[] args) {
